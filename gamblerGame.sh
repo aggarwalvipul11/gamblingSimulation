f81b07fc5753c5e1054f505283f40c4f54f3e7f0
aggarwalvipul11 <vipul_dev@DESKTOP-QA3S37M.localdomain>
2020-07-10 19:15:59 +0530

    [VipulA] ADD: Use Case 02, Gambler either win or loose.

--- gamblerGame.sh
+++ gamblerGame.sh
@@ -2,11 +2,14 @@
 
 echo "Welcome to the world of Gambling."
 
+# Assign variables and add values(Money).
 stakeMoneyPerDay=100;
 betMoneyPerGame=1;
 
+# Assign another variable and inside use RANDOM to generate numbers between 0 and 1.
 gameResult=$(($RANDOM%2))
 
+# Apply if condition and find if Gamble wins or loose.
 if [[ $gameResult -eq 1 ]]
 then
 	echo "Gambler Win"
@@ -15,3 +18,5 @@ else
 	echo "Gambler loose"
 	moneyEarns=$(($stakeMoneyPerDay-1))
 fi
+
+#End of Use Case 02
