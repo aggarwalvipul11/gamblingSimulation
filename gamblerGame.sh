05149e9b2e3b081d2a794b69cb6bb37f5b116ad9
aggarwalvipul11 <65864933+aggarwalvipul11@users.noreply.github.com>
2020-07-11 09:53:20 +0530

    Update gamblerGame.sh

--- gamblerGame.sh
+++ gamblerGame.sh
@@ -7,25 +7,25 @@ echo "Welcome to the world of Gambling."
 stakeMoneyPerDay=100;
 betMoneyPerGame=1;
 
-# Added variables for adding a limit for minimum money loose and maximum money wins.
+# Added variables for adding a limit for minimum money lost and maximum money wins.
 maxMoneyWinPerDay=150;
 minMoneyLoosePerDay=50;
 moneyEarns=$((stakeMoneyPerDay));
 
-# Apply while condition and checks the condition connects or not
+# Apply while condition and checks the condition mets or not
 while [[ $moneyEarns -le $maxMoneyWinPerDay && $moneyEarns -ge $minMoneyLoosePerDay ]]
 do
 	gameResult=$(($RANDOM%2));
 
-	# Apply if condition and if Gamble wins or loose.
+	# Apply if condition and if Gamble wins or lost.
 	if [[ $gameResult -eq 1 ]]
 	then
 		echo "Gambler Win"
 		((moneyEarns++));
 	else
-		echo "Gambler loose"
+		echo "Gambler lost"
 		((moneyEarns--));
 	fi
 done
 
-#End of Use Case 03
+#End of Use Case03
